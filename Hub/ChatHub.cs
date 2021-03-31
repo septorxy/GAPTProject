da94cbb0b2deaff099aea5492bf6f7765c512b68
kristinapace19 <78602459+kristinapace19@users.noreply.github.com>
2021-03-31 15:20:43 +0200

    Update ChatHub.cs
    fixing prev error

--- Hub/ChatHub.cs
+++ Hub/ChatHub.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
 
-namespace Testing
+namespace GAPTProj
 {
     public class ChatHub : Hub
     {
-         public Task BroadcastMessage(string type, object inMessage) =>
-            Clients.All.SendAsync("broadcastMessage", type, inMessage)
+        public Task BroadcastMessage(string type, object inMessage) =>
+            Clients.All.SendAsync("broadcastMessage", type, inMessage);
+
+       /* public Task Echo(string name, int x, int y) =>
+            Clients.Client(Context.ConnectionId)
+                   .SendAsync("echo", name, $"{x}, {y} (echo from server)");*/
     }
 }
