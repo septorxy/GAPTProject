25387ec0694c7955a89a18d7ccda5ee84bed2f31
kristinapace19 <78602459+kristinapace19@users.noreply.github.com>
2021-03-31 15:16:02 +0200

    Update site.js
    added sprite and message updates

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -13,19 +13,23 @@ connection.start()
     .then(() => startGame())
     .catch(error => console.error(error.message));
 
+connection.onclose(error => {
+    console.assert(connection.state === signalR.HubConnectionState.Disconnected);
+
+    var disconnectedPlayer = new newMessage(player.x, player.y, player.name);
+    connection.send('broadcastMessage', "disconnection", disconnectedPlayer);
+
+});
 
 
 //RECEIVING FROM HUB
-function bindConnectionMessage()
-{
-    var messageCallback = function (type, name, x, y)
-    {
+function bindConnectionMessage() {
+    var messageCallback = function (type, inMessage) {
         if (!type) return;
-        if (type === "newPlayer")
-        {
-            if (name == player.name) { return; }
-            color = getColor(name);
-            opponent[oCount] = new component(30, 30, color, 100, 60, "circle", name);
+        if (type === "newPlayer") {
+            if (inMessage.name == player.name) { return; }
+            color = getColor(inMessage.name);
+            opponent[oCount] = new component(30, 30, color, 100, 60, "circle", inMessage.name);
             //opponent[oCount].newPos();
             opponent[oCount].update();
             //opponent[oCount].update();
@@ -33,22 +37,30 @@ function bindConnectionMessage()
             //alert(opponent[oCount].gamearea.id);
             oCount++;
         }
-        if (type === "updatePlayer")
-        {
+        if (type === "updatePlayer") {
             var j;
-            for (j = 0; j < opponent.length; j++)
-            {
-                if(opponent[j].name == name)
-                {
-                    
-                        opponent[j].x = x;
-                        opponent[j].y = y;
-                        //alert(opponent[j].name);
-                    
+            for (j = 0; j < opponent.length; j++) {
+                if (opponent[j].name == inMessage.name) {
+
+                    opponent[j].newX = inMessage.x;
+                    opponent[j].newY = inMessage.y;
+                    opponent[j].speed = inMessage.speed;
+                    //alert(opponent[j].name);
+
                 }
             }
         }
-        
+
+        if (type === "disconnection") {
+            var j;
+            for (j = 0; j < opponent.length; j++) {
+                if (opponent[j].name == inMessage.name) {
+                    opponent = opponent.splice(j, 1);
+
+                }
+            }
+        }
+
     }
 
 
@@ -79,9 +91,15 @@ function startGame() {
     player = new component(30, 30, color, 60, 60, type, name);
     //alert("player created");
     //Sending to hub
-    connection.send('broadcastMessage', "newPlayer", player.name, player.x, player.y);
+     var newPlayerSend = new newMessage(player.x, player.y, player.name);
+    connection.send('broadcastMessage', "newPlayer", newPlayerSend);
+}
 
-    
+function newMessage(x, y, name, speed) {
+    this.x = x;
+    this.y = y;
+    this.name = name;
+    this.speed = speed;
 }
 
 var myGameArea = {
@@ -138,16 +156,23 @@ var myGameArea = {
     }
 }
 
-function component(width, height, color, x, y, type , name) {
+function component(width, height, color, x, y, type, name) {
     this.gamearea = myGameArea;
     this.width = width;
     this.height = height;
     this.speedX = 0;
     this.speedY = 0;
+    this.speed = 0;
     this.name = name;
     this.x = x;
     this.y = y;
+    this.newX = x;
+    this.newY = y;
     this.type = type
+    this.direction = "";
+    this.bool = false;
+
+
     if (this.type == "circle") {
         this.update = function () {
             ctx = myGameArea.context;
@@ -157,6 +182,174 @@ function component(width, height, color, x, y, type , name) {
             ctx.fill()
 
         }
+    }
+    else if (this.type == "sprite") {
+
+        this.update = function () {
+            ctx = myGameArea.context;
+            var img = new Image();
+            switch (this.direction) {
+                case "U":
+                    if (this.speed == 10) {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Up-warlock-runl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Up-warlock-runr.png";
+                        }
+                    }
+                    else {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Up-warlock-walkl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Up-warlock-walkr.png";
+                        }
+                    }
+                    break;
+                case "RU":
+                    if (this.speed == 10) {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/RU-warlock-runl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/RU-warlock-runr.png";
+                        }
+                    }
+                    else {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/RU-warlock-walkl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/RU-warlock-walkr.png";
+                        }
+                    }
+                    break;
+                case "LU":
+                    if (this.speed == 10) {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/LU-warlock-runl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/LU-warlock-runr.png";
+                        }
+                    }
+                    else {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/LU-warlock-walkl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/LU-warlock-walkr.png";
+                        }
+                    }
+                    break;
+                case "D":
+                    if (this.speed == 10) {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Down-warlock-runl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Down-warlock-runr.png";
+                        }
+                    }
+                    else {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Down-warlock-walkl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Down-warlock-walkr.png";
+                        }
+                    }
+                    break;
+                case "RD":
+                    if (this.speed == 10) {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/RD-warlock-runl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/RD-warlock-runr.png";
+                        }
+                    }
+                    else {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/RD-warlock-walkl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/RD-warlock-walkr.png";
+                        }
+                    }
+                    break;
+                case "LD":
+                    if (this.speed == 10) {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/LD-warlock-runl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/LD-warlock-runr.png";
+                        }
+                    }
+                    else {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/LD-warlock-walkl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/LD-warlock-walkr.png";
+                        }
+                    }
+                    break;
+                case "L":
+                    if (this.speed == 10) {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Left-warlock-runl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Left-warlock-runr.png";
+                        }
+                    }
+                    else {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Left-warlock-walkl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Left-warlock-walkr.png";
+                        }
+                    }
+                    break;
+                case "R":
+                    if (this.speed == 10) {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Right-warlock-runl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Right-warlock-runr.png";
+                        }
+                    }
+                    else {
+                        if (this.bool) {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Right-warlock-walkl.png";
+                        }
+                        else {
+                            img.src = "https://spritestorage.blob.core.windows.net/warlock/Right-warlock-walkr.png";
+                        }
+                    }
+                    break;
+                default:
+                    if (this.bool) {
+                        img.src = "https://spritestorage.blob.core.windows.net/warlock/Down-warlock-walkl.png";
+                    }
+                    else {
+                        img.src = "https://spritestorage.blob.core.windows.net/warlock/Down-warlock-walkl.png";
+                    }
+                    break;
+
+            }
+
+            ctx.drawImage(img, this.x, this.y, this.width, this.height);
+
+
+        }
+
+
     }
     else if (this.type == "rectangle") {
         this.update = function () {
@@ -170,6 +363,78 @@ function component(width, height, color, x, y, type , name) {
         this.y += this.speedY;
         connection.send('broadcastMessage', "updatePlayer", this.name, this.x, this.y);
 
+        var newPlayerUpdate = new newMessage(this.x, this.y, this.name, this.speed);
+        connection.send('broadcastMessage', "updatePlayer", newPlayerUpdate);
+
+    }
+}
+
+this.interpolate = function () {
+    var d = Math.hypot(this.newX - this.x, this.newY, this.y);
+
+    //find equation of line  y=mx+b
+
+    var m = (this.newY - this.y) / (this.newX - this.x);
+
+    var b = this.y - (m * this.x);
+
+
+
+    if (d > this.speed * 4 && this.speed != 0) {
+        var i;
+        for (i = 0; i < d / this.speed; i++) {
+            if (this.newX < this.x  && this.newY != this.y) {
+                this.x = this.x - this.speed/2;
+                this.y = (m * this.x) + b;
+                this.update();
+            }
+            else if (this.newX > this.x && this.newY != this.y) {
+                this.x = this.x + this.speed/2;
+                this.y = (m * this.x) + b;
+                this.update();
+            }
+            else if (this.newX < this.x ) {
+                this.x = this.x - this.speed;
+                this.y = (m * this.x) + b;
+                this.update();
+            }
+            else if (this.newX > this.x) {
+                this.x = this.x + this.speed;
+                this.y = (m * this.x) + b;
+                this.update();
+            }
+            else {
+                if (this.newY < this.y) {
+                    this.y = this.y - this.speed;
+                    this.update();
+                }
+                else if (this.newY > this.y) {
+                    this.y = this.y + this.speed;
+                    this.update();
+                }
+
+            }
+        }
+
+    }
+
+    this.x = this.newX;
+    this.y = this.newY;
+
+    this.update();
+
+}
+
+
+
+function MouseClick(event) {
+    if (event.button == 0) //left click
+    {
+        action("shoot");
+    }
+    else if (event.button == 2) //right click
+    {
+        action("ability");
     }
 }
 
@@ -177,6 +442,8 @@ function updateGameArea() {
     myGameArea.clear();
     player.speedX = 0;
     player.speedY = 0;
+    player.direction = "";
+
     if ((37 in keys || 65 in keys) && !((38 in keys || 87 in keys) || (40 in keys || 83 in keys))) {
         if (player.x - player.speedX > player.width) {
             if (16 in keys) {
@@ -186,6 +453,7 @@ function updateGameArea() {
                 player.speedX = -5;
             }
         }
+        player.direction = "L";
     }
     if ((39 in keys || 68 in keys) && !((38 in keys || 87 in keys) || (40 in keys || 83 in keys))) {
         if (player.x + player.speedX < myGameArea.canvas.width - player.width) {
@@ -196,6 +464,7 @@ function updateGameArea() {
                 player.speedX = 5;
             }
         }
+        player.direction = "R";
     }
     if ((38 in keys || 87 in keys) && !((65 in keys || 37 in keys) || (39 in keys || 68 in keys))) {
         if (player.y - player.speedY > player.height) {
@@ -206,6 +475,7 @@ function updateGameArea() {
                 player.speedY = -5;
             }
         }
+        player.direction = "U";
     }
     if ((40 in keys || 83 in keys) && !((65 in keys || 37 in keys) || (39 in keys || 68 in keys))) {
         if (player.y + player.speedY < myGameArea.canvas.height - player.height) {
@@ -216,6 +486,7 @@ function updateGameArea() {
                 player.speedY = 5;
             }
         }
+        player.direction = "D";
     }
 
     if ((38 in keys || 87 in keys) && (65 in keys || 37 in keys)) {
@@ -235,6 +506,7 @@ function updateGameArea() {
                 player.speedX = -5 / 2;
             }
         }
+        player.direction = "LU";
     }
 
     if ((38 in keys || 87 in keys) && (39 in keys || 68 in keys)) {
@@ -254,6 +526,7 @@ function updateGameArea() {
                 player.speedX = 5 / 2;
             }
         }
+        player.direction = "RU";
     }
 
     if ((40 in keys || 83 in keys) && (65 in keys || 37 in keys)) {
@@ -273,6 +546,7 @@ function updateGameArea() {
                 player.speedX = -5 / 2;
             }
         }
+        player.direction = "LD";
     }
     if ((40 in keys || 83 in keys) && (39 in keys || 68 in keys)) {
         if (player.y + player.speedY < myGameArea.canvas.height - player.height) {
@@ -291,17 +565,63 @@ function updateGameArea() {
                 player.speedX = 5 / 2;
             }
         }
+        player.direction = "RD";
+    }
+    if (16 in keys) {
+        player.speed = 10;
+    }
+    else {
+        player.speed = 5;
+    }
+
+
+    if (player.speedX != 0 || player.speedY != 0) {
+        if (walk >= 4) {
+            player.bool = !player.bool;
+            walk = 0;
+        }
+        else {
+            walk++;
+        }
+    }
+
+    if (82 in keys || 17 in keys) {
+        action("reload");
+    }
+
+    if (69 in keys || 13 in keys) {
+        action("interaction");
+    }
+
+    if (81 in keys || 32 in keys) {
+        action("changeWeapon");
     }
 
     player.newPos();
     player.update();
 
-    var j;
-    for (j = 0; j < opponent.length; j++)
-    {
-        opponent[j].update();
-    }
 
+    var j;
+    for (j = 0; j < opponent.length; j++) {
+        opponent[j].interpolate();
+    }
+}
+function action(actionType) {
+    if (actionType == "shoot") {
+        //shoot action
+    }
+    else if (actionType == "reload") {
+        //reload action
+    }
+    else if (actionType == "ability") {
+        //ability action
+    }
+    else if (actionType == "interaction") {
+        //interaction action
+    }
+    else if (actionType == "changeWeapon") {
+        //change weapon action
+    }
 }
 
 function getColor(name) {
@@ -328,4 +648,3 @@ function getColor(name) {
 
     return color;
 }
-
