81b33d06864fe347e5699d89ecf881b89839dc96
kristinapace19 <kristina.pace.19@um.edu.mt>
2021-04-22 20:04:09 +0200

    Phaser implemented! Beginning stage a few bugs but can incorporate other stuff with this version

--- Hub/ChatHub.cs
+++ Hub/ChatHub.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.SignalR;
 using System.Threading.Tasks;
+using System;
 
 namespace Testing
 {
     public class ChatHub : Hub
     {
-        public Task BroadcastMessage(string type, object inMessage) =>
-            Clients.All.SendAsync("broadcastMessage", type, inMessage);
+        public Task BroadcastMessage(string type, object inMessage) { 
+            Console.WriteLine(inMessage.ToString() + type);
+            return Clients.All.SendAsync("broadcastMessage", type, inMessage);
+        }
 
        /* public Task Echo(string name, int x, int y) =>
             Clients.Client(Context.ConnectionId)
