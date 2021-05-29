7c7eca2de144068b618d768be9e2e96834a126f4
LukeGrixti <61828249+LukeGrixti@users.noreply.github.com>
2021-05-29 21:51:41 +0200

    AdditionalComments

--- WanderingWarlocks/Controllers/GameController.cs
+++ WanderingWarlocks/Controllers/GameController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+/*Custom controller to control the views made for the game-related pages (Instructions and the game itself)*/
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,19 +14,24 @@ namespace WanderingWarlocks.Controllers
     {
         
 
-        [Authorize]
+        [Authorize] //User must be logged in to play the game
         public IActionResult Play()
         {
+            
+            /*A user who has never clicked play before will be redirected to the Instructions view
+              A cookie is then saved to indicate that the user has seen the instructions
+              If the cookie is present, the user will be taken directly to the game*/
+
             CookieOptions option = new CookieOptions();
             if (Request.Cookies["instrCookie"] != null)
             {
-                return View();
+                return View(); //Continue to the game normally if the cookie is present
             }
             else
             {
-                option.Expires = DateTime.Now.AddMonths(3);
-                Response.Cookies.Append("instrCookie", "seen", option);
-                return RedirectToAction("Instructions");
+                option.Expires = DateTime.Now.AddMonths(3); //Set cookie expiration to 3 months from creation
+                Response.Cookies.Append("instrCookie", "seen", option); //Append cookie to response
+                return RedirectToAction("Instructions"); //Redirect user to instructions page
             }
         }
 
