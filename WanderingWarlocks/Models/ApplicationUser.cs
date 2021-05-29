7e09b8676bca9f3f2417f00aa521d7396b049943
LukeGrixti <61828249+LukeGrixti@users.noreply.github.com>
2021-05-29 21:50:11 +0200

    AdditionalComments

--- WanderingWarlocks/Models/ApplicationUser.cs
+++ WanderingWarlocks/Models/ApplicationUser.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+/*Extending IdentityUser so as to add a Kills field
+ This is done for scalability, so that in the future each player can have total kills assignned to his account
+ It would also make it possible for an all-time hall of fame to be implemented, adding competitiveness and a fun aspect*/
+
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +13,7 @@ namespace WanderingWarlocks.Models
     public class ApplicationUser : IdentityUser
     {
 
-        [PersonalData]
+        [PersonalData] //Enabling the user to download their kill-count as part of their personal data
         public double Kills { get; set; }
     }
 }
